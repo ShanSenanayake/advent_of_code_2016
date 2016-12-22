40ca39a7360b1097c824c9002e7ac4ea8ce44f7e
Shan Senanayake <senanayake92@gmail.com>
2016-12-22 15:18:39 +0100

    day 4 part 2 done

--- advent_of_code/src/Day4.clj
+++ advent_of_code/src/Day4.clj
@@ -2,6 +2,9 @@
 
 (def input (clojure.string/split-lines (slurp "advent_of_code/inputs/day4.input")))
 
+(def letters (seq "abcdefghijklmnopqrstuvwxyz"))
+(def letter-to-num (zipmap letters (range (count letters))))
+
 (defn parse-line [line]
   (rest (re-matches #"(^[^\d]*)(\d+)\[([a-zA-Z]+)\]$" line)))
 
@@ -35,9 +38,28 @@
 (defn remove-decoys [list]
   (filter not-decoy? (map decrypt list)))
 
+(defn shift [steps char]
+  (nth letters (mod (+ steps (get letter-to-num char)) (count letters))))
+
+(defn shift-char [steps char]
+  (if (= char \-) \space (shift steps char)))
+
+(defn crack-cipher [[encrypt a steps b]]
+  [ (apply str (map (partial shift-char steps) encrypt))
+    steps])
+
+(defn find-north-pole [[decrypt id]]
+  (.contains decrypt "north" ))
+
+(defn filter-north-pole [list]
+  (first (filter find-north-pole (map crack-cipher list))))
+
 (defn -main
   "Fourth task of advent of code"
   [& args]
   (println "Day 4. Security Through Obscurity")
   (printf "##Answer: sum of valid checksums %d\n"
-    (apply + (for [[a b c d] (remove-decoys (parse-input input))] c))))
+    (apply + (for [[a b c d] (remove-decoys (parse-input input))] c)))
+  (println "Part 2")
+  (printf "##Answer: North pole is in rooom %d\n"
+    (last (filter-north-pole (remove-decoys (parse-input input))))))
